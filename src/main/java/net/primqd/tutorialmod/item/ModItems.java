db887899f596ef214ebda8151cbff6fc215023d2
Primqd <franklinvchen@outlook.com>
2024-11-30 00:12:10 -0800

    added fuel and food capabilities to the black_hole item :shrug:

--- src/main/java/net/primqd/tutorialmod/item/ModItems.java
+++ src/main/java/net/primqd/tutorialmod/item/ModItems.java
@@ -1,8 +1,14 @@
 package net.primqd.tutorialmod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -10,18 +16,34 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.primqd.tutorialmod.TutorialMod;
 
+import static net.minecraft.component.type.ConsumableComponents.food;
+
 public class ModItems {
-	private static Item registerItem(String name) {
-        Identifier id = Identifier.of(TutorialMod.MOD_ID, name);
-        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
-        Item.Settings settings = new Item.Settings().registryKey(key);
-        return Registry.register(Registries.ITEM, key, new Item(settings));
-	}
+	public static final ConsumableComponent BLACK_HOLE_CONSUMABLE_COMPONENT = food() // adds funny non-normal food effects i think
+			.consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 5 * 20, 0))) // time is in ticks
+			.consumeSeconds(6)
+			.build();
+	public static final FoodComponent BLACK_HOLE_FOOD_COMPONENT = new FoodComponent.Builder() // this makes it a food
+			.alwaysEdible()
+			.nutrition(5)
+			.saturationModifier(5)
+			.build();
 
 	public static void registerModItems() {
 		// Initializes all items created in this class.
 		TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
-        Item BLACK_HOLE = registerItem("black_hole");
+
+		// registers the black_hole item
+		Identifier itemId = Identifier.of(TutorialMod.MOD_ID, "black_hole");
+		RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, itemId);
+		Item.Settings settings = new Item.Settings().food(BLACK_HOLE_FOOD_COMPONENT, BLACK_HOLE_CONSUMABLE_COMPONENT).registryKey(key);
+		Item BLACK_HOLE = Registry.register(Registries.ITEM, key, new Item(settings));
+
+		// makes black_hole able to be used as fuel
+		FuelRegistryEvents.BUILD.register((builder, context) -> {
+			builder.add(BLACK_HOLE, context.baseSmeltTime()); // smelts 1 item
+		});
+
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
 			entries.add(BLACK_HOLE);
 		});
